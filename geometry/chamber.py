ad2cc4c88ab6e05eb0012a6be6dca29176fe50d6
intplonewolf <sarahzakiansari@gmail.com>
2026-03-28 11:04:32 +0100

    pep8

--- geometry/chamber.py
+++ geometry/chamber.py
@@ -1,25 +1,27 @@
-# import numpy as np
-
-
-# def chamber_length(rt, rc, L_star=1.0):
-
-#     At = np.pi * rt**2
-#     Ac = np.pi * rc**2
-
-#     return L_star * At / Ac
-
 import numpy as np
 
 
 def chamber_length(rt, rc, L_star=1.0, conv_length=0.0):
-
+    """
+    Calculates the cylindrical chamber length for a rocket motor.
+    
+    Args:
+        rt (float): Throat radius.
+        rc (float): Chamber radius.
+        L_star (float): Characteristic length (standard: L*).
+        conv_length (float): Length of the converging nozzle section.
+    """
+    # Areas: Throat (At) and Chamber (Ac)
     At = np.pi * rt**2
     Ac = np.pi * rc**2
 
+    # Volume of the converging section (frustum of a cone)
     V_conv = (np.pi * conv_length / 3.0) * (rc**2 + rc * rt + rt**2)
 
+    # Total required combustion volume based on L_star
     V_total = L_star * At
 
+    # Remaining volume required from the cylindrical section
     V_cyl = V_total - V_conv
 
     if V_cyl <= 0:
@@ -28,4 +30,5 @@ def chamber_length(rt, rc, L_star=1.0, conv_length=0.0):
             "Increase L_star or reduce conv_length."
         )
 
+    # Return L_cyl (Length of the cylindrical portion)
     return V_cyl / Ac
